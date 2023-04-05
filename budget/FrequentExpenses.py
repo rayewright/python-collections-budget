9732a25b9bb59dcac8619a7429341aed68c01045
Ray <ray.wright@sas.com>
2023-04-05 09:33:19 -0400

    Completed module

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,33 @@
+from . import Expense 
+import collections 
+import matplotlib.pyplot as plt
+
+
+expenses = Expense.Expenses()
+
+expenses.read_expenses("data/spending_data.csv")
+
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+spending_counter = collections.Counter(spending_categories)
+
+#print(spending_counter)
+
+top5 = spending_counter.most_common(5)
+
+#print('top 5 expenses:', top5)
+
+
+categories, count = zip(*top5)
+
+#print(categories)
+#print(count)
+
+fig, ax = plt.subplots() 
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+
+plt.show()
